dbbfbeeabf255cd6129f9aff28d308a5ef4fa58f
Sriram Srinivasan <43392814+sriram-srini00@users.noreply.github.com>
2020-04-27 10:45:43 -0400

    Create midi_message.py

---
+++ midi_message.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import mido
+from mido import MidiFile
+
+cwd_new = os.getcwd() + "/midi_audio"
+for midifile in os.listdir(cwd_new):
+    path = "{}/{}".format(cwd_new, midifile)
+    with open("{}/midi_messages/{}.txt".format(os.getcwd(), midifile), "w") as output:
+        mid = MidiFile(path)
+        for track in mid.tracks:
+            for msg in track:
+                output.write(str(msg) + "\n")
