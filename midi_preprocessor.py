c7c40b80ef0d1cecbd71a66f1b6df98e5d3f729a
Sriram Srinivasan <43392814+sriram-srini00@users.noreply.github.com>
2020-04-27 10:45:34 -0400

    Create midi_preprocessor.py

---
+++ midi_preprocessor.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pretty_midi
+import numpy as np
+
+cwd_new = os.getcwd() + "/midi_audio"
+for midifile in os.listdir(cwd_new):
+    path = "{}/{}".format(cwd_new, midifile)
+    with open("{}/midi_processed/{}.txt".format(os.getcwd(), midifile[:-4]), "w") as output:
+        midi_data = pretty_midi.PrettyMIDI(path)
+        midi_dict = {}
+        for instrument in midi_data.instruments:
+            for note in instrument.notes:
+                start = note.start
+                end = note.end
+                pitch = note.pitch
+                #velocity = note.velocity
+                midi_dict[(start, end)] = midi_dict.get((start, end), [])
+                midi_dict[(start, end)].append(pitch)
+            midi_list = sorted(midi_dict.items(), key = lambda key: key[0])
+            for item in midi_list:
+                output.write(str(item[0]) + " " + str(item[1]) + "\n")
